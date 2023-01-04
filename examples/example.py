99ee68222eb69f047c5414d7fe599befa664441a
Bruno Peixoto <brunolnetto@gmail.com>
2023-01-04 08:19:38 -0300

    refac/ review examples

--- examples/example.py
+++ examples/example.py
@@ -3,14 +3,30 @@ from pybary import bary_batch, bary_recursive
 from numpy import power, array
 from numpy.random import normal
 
+# Oracle function
 oracle = lambda x: power(x, 2)
+
+# Initial point
 x0 = array([0, 0])
 
-xhat_recursive = bary_recursive(oracle, x0, 10, 0.1, 0, 1, 100)
-
+# Batch points for batch barycenter version
 xs = normal(0, 1, [100, 2])
 
-xhat_batch = bary_recursive(oracle, xs, 10, 0.1)
+# Hyperparameters
+nu = 10
+sigma = 0.1
+zeta = 0
+lambda_ = 1
+iterations = 100
 
+# Recursive run
+xhat_recursive = bary_recursive(
+        oracle, x0, nu, sigma, zeta, lambda_, iterations
+    )
+
+# Batch run
+xhat_batch = bary_batch(oracle, xs, nu, sigma)
+
+# Results
 print(xhat_batch)
 print(xhat_recursive)
