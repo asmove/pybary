7d595cf4f200319fe32cd239213d2b29f020a06f
Bruno Peixoto <brunolnetto@gmail.com>
2023-01-04 20:20:38 -0300

    refac/ review examplas according to batch refac

--- examples/example.py
+++ examples/example.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 from pybary import bary_batch, bary_recursive
-from numpy import power, array
+from numpy import array
 from numpy.random import normal
+from numpy.linalg import norm
 
 # Oracle function
-oracle = lambda x: power(x, 2)
+oracle = lambda x: norm(x)
 
 # Initial point
 x0 = array([0, 0])
@@ -12,27 +13,23 @@ x0 = array([0, 0])
 # Batch points for batch barycenter version
 mu_x = 0
 sigma_x = 1
-size_x = [100, 2]
+size_x = [1000, 2]
 
 xs = normal(mu_x, sigma_x, size_x)
 
 # Hyperparameters
-nu = 10
+nu = 1
 sigma = 0.1
 zeta = 0
 lambda_ = 1
-iterations = 100
+iterations = 1000
 
 # Recursive run
-xhat_recursive = bary_recursive(
-        oracle, x0, nu, sigma, zeta, lambda_, iterations
-    )
+xhat_recursive = bary_recursive(oracle, x0, nu, sigma, zeta, lambda_, iterations)
 
 # Batch run
-xhat_batch = bary_batch(
-        oracle, xs, nu, sigma
-    )
+xhat_batch = bary_batch(oracle, xs, nu)
 
 # Results
-print(xhat_batch)
-print(xhat_recursive)
+print('Batch result     : '+str(xhat_batch))
+print('Recursive result : '+str(xhat_recursive))
