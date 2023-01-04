f3ef3d6971674c335f6453f13c7519c49bee9fe3
Bruno Peixoto <brunolnetto@gmail.com>
2023-01-03 22:04:17 -0300

    feat/ initialize tests folder

---
+++ tests/test_pybary.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+import pytest
+
+from pybary.pybary import bary_batch, bary_recursive
+
+
+def test_bary():
+    """
+    Generates a tuple with key-value
+    """
+    assert 'banana' == 'banana'
+
+
